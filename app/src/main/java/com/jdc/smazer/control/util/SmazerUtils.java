fb0872899a2844e8ecb2158e247318f28e1f2287
jhondev-code <jhondev.code@gmail.com>
2021-01-02 11:14:27 -0500

    Agregue la clase de utilidades SmazerUtil.

---
+++ app/src/main/java/com/jdc/smazer/control/util/SmazerUtils.java
@@ -0,0 +1,28 @@
+package com.jdc.smzer.control.util;
+
+import java.awt.Desktop;
+
+import java.net.URI;
+
+import org.tinylog.Logger;
+
+/**
+ * Esta clase contiene diversas utilidades para funciones especificas.
+ *
+ * @author jhon fernandez
+ * @version 1.0.0
+ */
+public class SmazerUtils {
+
+    /**
+     * Este metodo es usado para poder abrir una url en el navegador por defecto.
+     */
+    public static void browse(String url) {
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch(Exception ex) {
+            Logger.log(ex);
+        }
+    }
+
+}
